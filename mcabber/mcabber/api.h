f91e5eca47fec2ae087758628d4b164b14c6dacd
Mikael Berthe <mikael@lilotux.net>
2010-03-27 10:48:36 +0100

    Update autotools install script and documentation

---
+++ mcabber/mcabber/api.h
@@ -0,0 +1,13 @@
+#ifndef __MCABBER_API_H__
+#define __MCABBER_API_H__ 1
+
+#include <mcabber/config.h> // For MCABBER_BRANCH
+
+#define MCABBER_API_VERSION 12
+#define MCABBER_API_MIN     11
+
+extern const gchar *mcabber_branch;
+extern const guint mcabber_api_version;
+
+#endif
+/* vim: set expandtab cindent cinoptions=>2\:2(0 sw=2 ts=2:  For Vim users... */
